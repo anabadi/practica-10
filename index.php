05bc2357fea4203eac7b7e6deed6d641accc0db0
anabadi <anabadi1804@gmail.com>
2017-03-14 09:31:18 -0600

    1
    php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+//comentarios
+/*comentarios
+multilineas*/
+#comentarios
+echo "hola mundo php";
+
+?>
+
+<html>
+<head>
+<title>Practica10</title>
+
+</head>
+<body>
+<h1> Hola mundo html</h1>
+
+</body>
+
+</html>
---
+++ index2.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>Practica10</title>
+
+</head>
+<body>
+
+   <h1>usando PHP</h1>
+   <p><?php 
+     echo "parrafo php";
+	 ?></p>
+     <p>parrafo html</p>
+      <h3><?php 
+     echo "parrafo php";
+	 ?></h3>
+	 </body>
+
+</html>
